df5b15692cfcd4ca16cfc4cb4628f454131c73e1
Slawomir Leski <slawomir.leski@idnow.de>
2016-05-19 16:27:38 +0200

    Commit no1

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,4 +3,9 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
+	
+	public static void test() {
+		System.out.println("Test!");
+	}
+	
 }
