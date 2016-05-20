3657149817f3f8015a01d41b2f3758f2ee4075dc
Slawomir Leski <slawomir.leski@idnow.de>
2016-05-20 13:05:36 +0200

    New commit

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,6 +3,11 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
+
+	enum Type {
+		ONE,
+		TWO
+	}
 	
 	public static void test() {
 		System.out.println("Test!");
